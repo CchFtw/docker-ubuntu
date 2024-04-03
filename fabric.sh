8d98c4eb3ed8110ccd7abd7fdda3857e88ddd316
CchFtw <165870473+CchFtw@users.noreply.github.com>
2024-04-03 13:17:24 +0530

    Update and rename Install-fabric.sh to fabric.sh

--- fabric.sh
+++ fabric.sh
@@ -1,4 +1,5 @@
-docker run -d -v /sarver:/data \
+docker run -d -it -v /sarver:/data \
     -e VERSION=1.20.4 \
     -e TYPE=FABRIC \
     -p 25565:25565 -e EULA=TRUE --name mc itzg/minecraft-server
+echo "docker attach mc" for console
